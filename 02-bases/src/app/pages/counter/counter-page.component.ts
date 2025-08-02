8d51f55440c93ccf730974637b5a818405eb854f
GuidoFC <guidofigueroa96@gmail.com>
2025-08-02 16:26:07 +0200

    v-37 creamos una señal. Y con el metodo set, reseteamos esa señal. Y con el metodo update podemos obtener el valor actual de esa señal y modificarla, en tese caso sumando o restando 1

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,16 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, signal} from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html'
 })
 export class CounterPageComponent{
 counter = 10;
+counterSignal = signal(10)
 
   increment(value: number) {
     this.counter += value;
+    this.counterSignal.update((current) => current + value)
   }
 
   reset() {
-    this.counter =10 ;
+    this.counter =0 ;
+    this.counterSignal.set(0);
   }
 }
