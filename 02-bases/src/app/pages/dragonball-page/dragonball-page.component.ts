6e088d45dd7b12b0b889f697e1fb8079bb23135a
GuidoFC <guidofigueroa96@gmail.com>
2025-08-20 12:04:00 +0200

    v-51 recordando a como usar el @for

--- 02-bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -8,4 +14,9 @@ import { Component } from '@angular/core';
 })
 export class DragonballPageComponent {
 
+  characters = signal<Character[]>([
+    {id: 1, name: "Goku", power: 1000},
+    {id: 2, name: "Vegeta", power: 9999},
+    {id: 3, name: "Piccolo", power: 500},
+  ])
 }
