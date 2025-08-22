5d8094fdaa1210da7602ec43504410eec63bdec0
GuidoFC <guidofigueroa96@gmail.com>
2025-08-22 16:55:30 +0200

    v-58 creamos nuevo compoente

---
+++ 02-bases/src/app/compontents/dragonBall/dragonball-add/dragonball-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-add',
+  imports: [],
+  templateUrl: './dragonball-add.component.html',
+  styles: ``
+})
+export class DragonballAddComponent {
+
+}
