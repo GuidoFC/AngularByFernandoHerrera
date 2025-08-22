2abdc820e7e9abe241c685f6a5e55f5eca9ad6be
GuidoFC <guidofigueroa96@gmail.com>
2025-08-22 12:56:59 +0200

    v-56 creamos una copia del componente

---
+++ 02-bases/src/app/pages/Copy-dragonball-page/copy-dragonball-page.component.ts
@@ -0,0 +1,54 @@
+import {Component, signal} from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [],
+  templateUrl: './copy-dragonball-page.component.html',
+  styles: ``
+})
+export class CopyDragonballPageComponent {
+
+
+  name = signal<string>("");
+  power = signal<number>(0);
+
+  listCharacters = signal<Character[]>([
+    {id: 1, name: "Goku", power: 9001},
+    {id: 2, name: "Vegeta", power: 9000},
+    {id: 3, name: "Piccolo", power: 8000},
+    {id: 4, name: "Yamcha", power: 300},
+  ])
+
+  addNewCharacter() {
+
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      console.log("No podemos guardar nada")
+      return
+    }
+
+    const newCharacter: Character = {
+      id: this.listCharacters().length + 1,
+      name: this.name(),
+      power: this.power()
+
+    }
+
+    this.listCharacters.update(values => {
+      return [...values, newCharacter];
+    });
+
+    this.resetFilds();
+  }
+
+  resetFilds() {
+    this.power.set(0)
+    this.name.set("")
+  }
+
+}
