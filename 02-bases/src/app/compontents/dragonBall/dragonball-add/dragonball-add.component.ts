0e7db620170aa099a4f509edbba512cfe5cff84c
GuidoFC <guidofigueroa96@gmail.com>
2025-08-23 16:32:49 +0200

    v-58 hemos creado un componente para poder crear un nuevo personaje. En el siguiente video aprenderemos a coger la información de un componente hijp para que le envie la información al componente padre y este supongo que se lo dara al componete hijo

--- 02-bases/src/app/compontents/dragonBall/dragonball-add/dragonball-add.component.ts
+++ 02-bases/src/app/compontents/dragonBall/dragonball-add/dragonball-add.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {Character} from '../../../interfaces/character.interfaces';
 
 @Component({
-  selector: 'app-dragonball-add',
+  selector: 'dragonball-add',
   imports: [],
+  standalone: true,
   templateUrl: './dragonball-add.component.html',
   styles: ``
 })
 export class DragonballAddComponent {
 
+  name = signal<string>("");
+  power = signal<number>(0);
+
+  addNewCharacter() {
+
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      console.log("No podemos guardar nada")
+      return
+    }
+
+    const newCharacter: Character = {
+      id: 100,
+      name: this.name(),
+      power: this.power()
+
+    }
+
+    // this.listCharacters.update(values => {
+    //   return [...values, newCharacter];
+    // });
+
+    console.log("nuevo personaje: " + JSON.stringify(newCharacter))
+
+    this.resetFilds();
+  }
+
+  resetFilds() {
+    this.power.set(0)
+    this.name.set("")
+  }
 }
--- 02-bases/src/app/pages/Copy-dragonball-page/copy-dragonball-page.component.ts
+++ 02-bases/src/app/pages/Copy-dragonball-page/copy-dragonball-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, signal} from '@angular/core';
 import {CharacterListComponent} from '../../compontents/dragonBall/character-list/character-list.component';
 import {Character} from '../../interfaces/character.interfaces';
+import {DragonballAddComponent} from '../../compontents/dragonBall/dragonball-add/dragonball-add.component';
 
 
 
@@ -8,44 +9,19 @@ import {Character} from '../../interfaces/character.interfaces';
   selector: 'copy-dragonball-page',
   templateUrl: './copy-dragonball-page.component.html',
   standalone: true,
-  imports: [CharacterListComponent],
+  imports: [CharacterListComponent, DragonballAddComponent],
   styles: ``
 })
 export class CopyDragonballPageComponent {
 
 
-  name = signal<string>("");
-  power = signal<number>(0);
+
 
   listCharacters = signal<Character[]>([
     {id: 1, name: "Goku", power: 9001},
     {id: 2, name: "Vegeta", power: 9000},
   ])
 
-  addNewCharacter() {
 
-    if (!this.name() || !this.power() || this.power() <= 0) {
-      console.log("No podemos guardar nada")
-      return
-    }
-
-    const newCharacter: Character = {
-      id: this.listCharacters().length + 1,
-      name: this.name(),
-      power: this.power()
-
-    }
-
-    this.listCharacters.update(values => {
-      return [...values, newCharacter];
-    });
-
-    this.resetFilds();
-  }
-
-  resetFilds() {
-    this.power.set(0)
-    this.name.set("")
-  }
 
 }
