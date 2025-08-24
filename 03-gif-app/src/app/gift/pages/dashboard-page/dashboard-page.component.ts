ed81824faf66d077ae29f6d3ae2127b5edf61006
GuidoFC <guidofigueroa96@gmail.com>
2025-08-24 17:06:39 +0200

    v-69 creamos la pagina

---
+++ 03-gif-app/src/app/gift/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  standalone: true,
+  templateUrl: './dashboard-page.component.html',
+  styles: ``
+})
+export class DashboardPageComponent {
+
+}
