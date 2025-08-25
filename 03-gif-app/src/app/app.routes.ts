f51e2ba563580b40a843d4d354a5c561299e9e9a
GuidoFC <guidofigueroa96@gmail.com>
2025-08-25 03:53:27 +0200

    v-69 ahora hemos hecho que nuestra page sea un layout para las otras paginas

--- 03-gif-app/src/app/app.routes.ts
+++ 03-gif-app/src/app/app.routes.ts
@@ -3,16 +3,19 @@ import {Routes} from '@angular/router';
 export const routes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./gift/pages/dashboard-page/dashboard-page.component')
+    loadComponent: () => import('./gift/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gift/pages/trending-page/trending-page.component')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gift/pages/search-page/search-page.component')
+      },
+    ],
   },
-  {
-    path: 'trending',
-    loadComponent: () => import('./gift/pages/trending-page/trending-page.component')
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('./gift/pages/search-page/search-page.component')
-  },
+
   {
     path: '**',
     redirectTo: 'dashboard',
