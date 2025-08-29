2cb031ccf2e17195edbe89410142747f76c6d6da
GuidoFC <guidofigueroa96@gmail.com>
2025-08-29 14:40:08 +0200

    v-73 como usar los alias de un path

--- 03-gif-app/src/app/gift/components/side-menu/side-menu-header/gift-side-menu-header.component.ts
+++ 03-gif-app/src/app/gift/components/side-menu/side-menu-header/gift-side-menu-header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {environment} from '../../../../../environments/environment';
+import {environment} from '@enviroment/environment';
+// import {environment} from '../../../../../environments/environment';
 
 @Component({
   selector: 'gift-side-menu-header',
