240f1091453db738ef63fa971e44d04433ad1c26
GuidoFC <guidofigueroa96@gmail.com>
2025-08-29 11:51:39 +0200

    v-7 hemos creado un array de enalces para luego poder iterarlo. Primero hemos definido una interficie diciendo las carateristicas que tendra el enlace, luego lo hemos definido en el archivo .ts y finalmente lo hemos iterado con un for en el html

--- 03-gif-app/src/app/gift/components/side-menu/side-menu-options/gift-side-menu-options.component.ts
+++ 03-gif-app/src/app/gift/components/side-menu/side-menu-options/gift-side-menu-options.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+interface menuOption {
+  label: string
+  sublabel: string
+  icon: string
+  route: string
+}
 
 @Component({
   selector: 'gift-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   standalone: true,
   templateUrl: './gift-side-menu-options.component.html',
   styles: ``
 })
 export class GiftSideMenuOptionsComponent {
 
+  linkMenuOption: menuOption[] = [
+    {
+      label: 'Search',
+      sublabel: 'Busqueda',
+      icon: 'fa-solid fa-magnifying-glass',
+      route: '/dashboard/search'
+    },
+    {
+      label: 'Treding',
+      sublabel: 'Mas popular',
+      icon: 'fa-solid fa-fire',
+      route: '/dashboard/trending'
+    }
+  ]
 }
