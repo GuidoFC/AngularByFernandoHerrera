5bb738f5d087962461bdb717e63c0ebac0fc3443
GuidoFC <guidofigueroa96@gmail.com>
2025-08-29 15:09:47 +0200

    v-74 creando componentes

---
+++ 03-gif-app/src/app/gift/components/gift-list/gift-list-item/gift-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gift-list-item',
+  imports: [],
+  templateUrl: './gift-list-item.component.html',
+  styles: ``
+})
+export class GiftListItemComponent {
+
+}
---
+++ 03-gif-app/src/app/gift/components/gift-list/gift-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gift-list',
+  imports: [],
+  templateUrl: './gift-list.component.html',
+  styles: ``
+})
+export class GiftListComponent {
+
+}
